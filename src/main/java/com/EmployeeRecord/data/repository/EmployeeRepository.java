9704dd8784022dbffed83496785d96ab3e43eb0a
Yetife <tifetreats21@gmail.com>
2022-02-09 00:23:16 +0100

    Added employee record repository

---
+++ src/main/java/com/EmployeeRecord/data/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.EmployeeRecord.data.repository;
+
+import com.EmployeeRecord.data.models.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    Optional<Employee> findByEmail(String email);
+}
