ed597033b067304d55dd11405e5e50e6afb00cf2
Yetife <tifetreats21@gmail.com>
2022-02-09 00:26:16 +0100

    Added employee record file

---
+++ src/main/resources/db/insert.sql
@@ -0,0 +1,9 @@
+
+truncate table employee;
+
+insert into employee (id, first_name, last_name, email, designation)
+values (11, 'Yetunde', 'Olasehinde', 'olasehinde@gmail.com', 'CEO'),
+(12, 'Boluwatife', 'Owolabi', 'bolu@gmail.com', 'Web develpoer'),
+(13, 'Tolani', 'Adekanmi', 'tolani@gmail.com', 'Data analyst'),
+(14, 'Stephen', 'Oluwakayode', 'stephen@gmail.com', 'HR consultant'),
+(15, 'Bukola', 'Afeni', 'bukky@gmail.com', 'IT Admin');
