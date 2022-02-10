ae77d7345ccef0b4a4816c6aea9add17904deec6
Yetife <tifetreats21@gmail.com>
2022-02-10 09:06:09 +0100

    Added employee record file

--- src/main/java/com/EmployeeRecord/service/employee/EmployeeService.java
+++ src/main/java/com/EmployeeRecord/service/employee/EmployeeService.java
@@ -4,6 +4,7 @@ import com.EmployeeRecord.data.models.Employee;
 import com.EmployeeRecord.data.models.dto.EmployeeDto;
 import com.EmployeeRecord.web.exceptions.EmployeeDoesNotExistException;
 import com.EmployeeRecord.web.exceptions.EmployeeLogicException;
+import com.github.fge.jsonpatch.JsonPatch;
 
 import java.util.List;
 
@@ -12,6 +13,6 @@ public interface EmployeeService {
     List<Employee> getAllEmployeeRecord();
     Employee addEmployee(EmployeeDto employeeDto) throws EmployeeLogicException;
     Employee findEmployeeById(Long employeeId) throws EmployeeDoesNotExistException;
-    Employee updateEmployeeRecord(Long employeeId, EmployeeDto employeeDto);
+    Employee updateEmployeeRecord(Long employeeId, JsonPatch employeePatch) throws EmployeeLogicException;
 
 }
